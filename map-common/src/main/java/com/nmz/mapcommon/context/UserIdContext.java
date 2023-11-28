39dfb2be98c8e1b2d19e257e39bdf4be2e41ffd5
niemingzhi <1094290505@qq.com>
2023-11-28 21:44:41 +0800

    refactor: 修改保存用户id工具类，引入ThreadLocalUtil用来存储多种类型数据

--- map-common/src/main/java/com/nmz/mapcommon/context/UserIdContext.java
+++ map-common/src/main/java/com/nmz/mapcommon/context/UserIdContext.java
@@ -1,23 +1,26 @@
 package com.nmz.mapcommon.context;
 
+import com.nmz.mapcommon.utils.ThreadLocalUtil;
+
 /**
  * @Description:
  * @Author: 聂明智
  * @Date: 2023/11/28-19:21
  */
 public class UserIdContext {
-    private static ThreadLocal<Long> userId = new ThreadLocal<>();
+
+    private static final String USER_ID = "userId";
 
     public static Long getUserId() {
-        return userId.get();
+        return ThreadLocalUtil.get(USER_ID);
     }
 
-    public static void setUserId(Long id) {
-        userId.set(id);
+    public static void setUserId(Long userId) {
+        ThreadLocalUtil.set(USER_ID, userId);
     }
 
     public static void remove(){
-        userId.remove();
+        ThreadLocalUtil.removeKey(USER_ID);
     }
 
 }
---
+++ map-common/src/main/java/com/nmz/mapcommon/utils/ThreadLocalUtil.java
@@ -0,0 +1,55 @@
+package com.nmz.mapcommon.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+
+public class ThreadLocalUtil {
+
+    private ThreadLocalUtil() {
+    }
+
+    private static final ThreadLocal<Map<Object, Object>> MAP_THREAD_LOCAL = ThreadLocal.withInitial(HashMap::new);
+
+    private static final Map<Object, Object> cacheMap;
+
+    static {
+        cacheMap = MAP_THREAD_LOCAL.get();
+    }
+
+    public static Map<Object, Object> getThreadLocal() {
+        return MAP_THREAD_LOCAL.get();
+    }
+
+    public static <T> T get(Object key) {
+        if (cacheMap == null) {
+            return null;
+        }
+        return (T) cacheMap.get(key);
+    }
+
+    public static <T> T get(Object key, Object defaultValue) {
+        if (cacheMap == null) {
+            return (T) defaultValue;
+        }
+        return (T) cacheMap.get(key);
+    }
+
+    public static Object set(Object key, Object value) {
+        return cacheMap.put(key, value);
+    }
+
+    public static void set(Map<Object, Object> map) {
+        cacheMap.putAll(map);
+    }
+
+    public static void removeKey(Object key) {
+        MAP_THREAD_LOCAL.remove();
+        if (!Objects.isNull(cacheMap.get(key))) {
+            cacheMap.remove(key);
+        }
+    }
+
+}
+
