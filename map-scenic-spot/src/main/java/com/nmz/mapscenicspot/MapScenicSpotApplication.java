8669e7ec5bea79db51aa6df9bd93192a9d3d0d06
niemingzhi <1094290505@qq.com>
2023-12-19 15:22:01 +0800

    feat: 增加景点模块

---
+++ map-scenic-spot/src/main/java/com/nmz/mapscenicspot/MapScenicSpotApplication.java
@@ -0,0 +1,13 @@
+package com.nmz.mapscenicspot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapScenicSpotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MapScenicSpotApplication.class, args);
+    }
+
+}
